d38c7d66c3c8fbd926214f9208484458c400f192
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-11-04 15:53:28 +0700

    Promotion Reduer

---
+++ redux/reducer/promotionsReducer.ts
@@ -0,0 +1,15 @@
+
+import * as ActionTypes from '../ActionTypes';
+
+export const promotionsReducer = (state = { isLoading: true, errMess: null, promotions: [] }, action: any) => {
+    switch (action.type) {
+        case ActionTypes.ADD_PROMOS:
+            return { ...state, isLoading: false, errMess: null, promotions: action.payload };
+        case ActionTypes.PROMOS_LOADING:
+            return { ...state, isLoading: true, errMess: null, promotions: [] }
+        case ActionTypes.PROMOS_FAILED:
+            return { ...state, isLoading: false, errMess: action.payload };
+        default:
+            return state;
+    }
+};
