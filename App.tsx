3b0faa5901086e84da1008a9dbd83c85d7ba61cc
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-11-04 15:55:46 +0700

    use Redux Provider

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-redux'
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+import { ConfigureStore } from './redux/ConfigureStore'
+const store = ConfigureStore();
 
+// Component Connect to redux has to come over the connect()[inside Redux] function 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Main></Main>
+    </Provider>
+    // <View style={styles.container}>
+    // <View >
+
+    // </View>
   );
 }
 
